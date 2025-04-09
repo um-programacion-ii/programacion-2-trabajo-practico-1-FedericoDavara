643c74c1edf7c39f39d81284bb030fedcf491019
FedericoDavara <f.davara@alumno.um.edu.ar>
2025-04-09 03:41:38 -0300

    Subclase Camion Creada

--- TP1_Gestion_de_vehiculos/src/Main.java
+++ TP1_Gestion_de_vehiculos/src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
         Vehiculo v1 = new Vehiculo("ABC123", "Ford", 2020, 1500);
         Vehiculo v2 = new Vehiculo("DEF456", "Chevrolet", 2018, 1300);
-        Vehiculo v3 = new Vehiculo("GHI789", "Toyota", 2022, 1000);
+        Camion camion = new Camion("CAM789", "Volvo", 2015, 8000, true);
 
         VehiculoPrinter printer = new VehiculoPrinter();
         printer.imprimir(v1);
         printer.imprimir(v2);
-        printer.imprimir(v3);
+        printer.imprimir(camion);
     }
 }
--- TP1_Gestion_de_vehiculos/src/VehiculoPrinter.java
+++ TP1_Gestion_de_vehiculos/src/VehiculoPrinter.java
@@ -1,8 +1,15 @@
 public class VehiculoPrinter {
     public void imprimir(Vehiculo vehiculo) {
-        System.out.println("Vehículo: " + vehiculo.getMarca()
+        System.out.print("Vehículo: " + vehiculo.getMarca()
                 + " | Patente: " + vehiculo.getPatente()
                 + " | Año: " + vehiculo.getAnio()
                 + " | Carga: " + vehiculo.getCapacidadCargaKg() + " kg");
+
+        if (vehiculo instanceof Camion) {
+            Camion camion = (Camion) vehiculo;
+            System.out.print(" | Acoplado: " + (camion.isTieneAcoplado() ? "Sí" : "No"));
+        }
+
+        System.out.println();
     }
 }
